83cb28a188577f743708af7ff126e25fac437c8c
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-13 17:06:24 -0500

    use request

---
+++ github.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+import requests
+import datetime, pprint
+
+client = MongoClient()
+db = client.test_database
+collection = db.github_accts
+
+def find_user(username):
+    response = requests.get(f'https://api.github.com/users/{username}')
+    json = response.json()
+    user_obj = {"username": username}
+    user_obj["bio"] = json.get('bio')
+    return user_obj
+
+def store_user(user_obj):
+    collection.insert_one(user_obj)
+
+print(find_user('sschneeberg'))
+print(collection.find_one())
