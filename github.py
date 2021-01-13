dd3eb12473319faaf77dfbb0649d555f493dc256
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-13 17:10:50 -0500

    use pymongo

--- github.py
+++ github.py
@@ -4,13 +4,14 @@ import datetime, pprint
 
 client = MongoClient()
 db = client.test_database
-collection = db.github_accts
+collection = db.githubaccts
 
 def find_user(username):
     response = requests.get(f'https://api.github.com/users/{username}')
     json = response.json()
     user_obj = {"username": username}
     user_obj["bio"] = json.get('bio')
+    store_user(user_obj)
     return user_obj
 
 def store_user(user_obj):
